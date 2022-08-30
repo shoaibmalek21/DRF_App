e8ee1966ede4879c32a4112a3e0570b8f15b9f3d
Shoaib <shoaibmalek21@gmail.com>
2022-08-30 12:04:35 +0530

    Shoaib commit

--- solutionAPI/user/serializers.py
+++ solutionAPI/user/serializers.py
@@ -17,3 +17,13 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
         return user
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+
+    """
+    Serializer for password change endpoint.
+    """
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
--- solutionAPI/user/urls.py
+++ solutionAPI/user/urls.py
@@ -1,4 +1,4 @@
-from .views import RegisterAPI, LoginAPI
+from .views import RegisterAPI, LoginAPI, ChangePasswordView
 from django.urls import path
 from knox import views as knox_views
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('api/login/', LoginAPI.as_view(), name='login'),
     path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+
+    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
+
 ]
